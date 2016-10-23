3d8fd952e1be916c1639a73670377732948a398c
AshidaSRS <ashida.shin@gmail.com>
2016-10-23 04:02:09 +0200

    [FEATURE] Add download method

--- idun.py
+++ idun.py
@@ -1,10 +1,13 @@
+# - * - coding: UTF-8 - * -
 import telebot
-import os.path as path
+import os
 import sys
-from subprocess import call
+import time
+import string
+import random
 
 # Create bot with its token
-if not path.isfile("bot.token"):
+if not os.path.isfile("bot.token"):
     print("Error: \"bot.token\" not found!")
     sys.exit()
 
@@ -19,13 +22,51 @@ bot.skip_pending = True
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "Soy el puto bot de idun")
+    bot.send_message(message.chat.id, "Soy Idhún, dame gelatina")
 
 @bot.message_handler(commands=['miautube'])
 def send_video(message):
-    call("ytb" + message.text)
-    bot.send_message(message.chat.id, "MiauTube")
+    bot.send_message(message.chat.id, "[MiauTube] *mordisco*")
+    mordisco = open('./mordisco.mp4', 'rb')
+    bot.send_document(message.chat.id, mordisco)
+    time_before = time.time()
+    os.system("youtubeplayer " + message.text.split()[1])
+    time_after = time.time()
+    if (time_after - time_before < 30):
+        bot.send_message(message.chat.id, "[MiauTube] Trying hard mode")
+	hardMode(message.text.split()[1])
+        bot.send_message(message.chat.id, "[MiauTube] HardMode success")
+    else:
+        bot.send_message(message.chat.id, "[MiauTube] Over")
+
+@bot.message_handler(commands=['miautwitch'])
+def send_video(message):
+    bot.send_message(message.chat.id, "[MiauTwitch] *arañazo*")
+    os.system("twitchtv " + message.text.split()[1] + " " + message.text.split()[2])
+    bot.send_message(message.chat.id, "[MiauTwitch] Over")
+
+def hardMode(message):
+    #TODO: Move to bin
+	code = message.split("=")[1]
+        random="&r=" + key_gen() + "&hd=1"
+        r = os.system("wget --auth-no-challenge http://www.youtubeinmp4.com/redirect.php?video=" + code + random)
+        video = "redirect.php?video=" + code
+        if (r == 0):
+            os.system("omxplayer "+ video)
+            os.system("rm " + video)
+        else:
+            print("Meh")
+
+
+KEY_LEN = 48
+
+def base_str():
+    return (string.letters+string.digits)
+def key_gen():
+    keylist = [random.choice(base_str()) for i in range(KEY_LEN)]
+    return ("".join(keylist))
+
 
 # Start the bot
-print("Running...")
+print("Nya...")
 bot.polling()
